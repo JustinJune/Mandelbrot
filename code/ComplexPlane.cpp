daa2890b9b16394930712fd1475596ef2706cc6c
admin <admin@BCS-Ubuntu-04.lrccd.ad.losrios.edu>
2022-09-28 11:45:52 -0700

    9/28 lab

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -4,7 +4,62 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 
 using namespace sf;
 using namespace std;
 
+ComplexPlane::ComplexPlane(float aspectRatio)
+{
+    m_aspectRatio = aspectRatio;
+    m_view.setSize(BASE_WIDTH, BASE_HEIGHT * m_aspectRatio);
+    m_view.setCenter(0.0, 0.0);
+    m_zoomCount = 0;
+}
+
+void ComplexPlane::zoomIn()
+{
+    m_zoomCount++;
+    float x_size, y_size;
+
+    x_size = BASE_WIDTH * pow(BASE_ZOOM, m_zoomCount);
+    y_size = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_zoomCount);
+
+    m_view.setSize(x_size, y_size);
+}
+
+void ComplexPlane::zoomOut()
+{
+    m_zoomCount--;
+    float x_size, y_size;
+
+    x_size = BASE_WIDTH * pow(BASE_ZOOM, m_zoomCount);
+    y_size = BASE_HEIGHT * m_aspectRatio * pow(BASE_ZOOM, m_zoomCount);
+
+    m_view.setSize(x_size, y_size);
+}
+
+void ComplexPlane::setCenter(Vector2f coord)
+{
+    m_view.setCenter(coord);
+}
+
+void ComplexPlane::setMouseLocation(Vector2f coord)
+{
+    m_mouseLocation = coord;
+}
+
+void ComplexPlane::loadText(Text &text)
+{
+
+}
+
+size_t ComplexPlane::countIterations(Vector2f coord)
+{
+
+}
+
+void ComplexPlane::iterationsToRGB(size_t count, Uint8 &r, Uint8 &g, Uint8 &b)
+{
+    
+}
--- code/ComplexPlane.h
+++ code/ComplexPlane.h
@@ -9,24 +9,31 @@
 using namespace sf;
 using namespace std;
 
+const unsigned int MAX_ITER = 64;
+const float BASE_WIDTH = 4.0;
+const float BASE_HEIGHT = 4.0;
+const float BASE_ZOOM = 0.5;
+
 class ComplexPlane
 {
     public:
-    ComplexPlane(float aspectRatio);
-    void zoomIn();
-    void zoomOut();
-    void setCenter(Vector2f coord);
-    View getView();
-    void setMouseLocation(Vector2f coord);
-    void loadText(Text& text);
-    size_t countIterations(Vector2f coord);
-    void iteratioonsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
+
+        ComplexPlane(float aspectRatio);
+        void zoomIn();
+        void zoomOut();
+        void setCenter(Vector2f coord);
+        View getView();
+        void setMouseLocation(Vector2f coord);
+        void loadText(Text& text);
+        size_t countIterations(Vector2f coord);
+        void iterationsToRGB(size_t count, Uint8& r, Uint8& g, Uint8& b);
 
     private:
-    Vector2f m_mouseLocation;
-    View m_view;
-    int m_zoomCount;
-    float m_aspectRatio;
+
+        Vector2f m_mouseLocation;
+        View m_view;
+        int m_zoomCount;
+        float m_aspectRatio;
 
 
 };
